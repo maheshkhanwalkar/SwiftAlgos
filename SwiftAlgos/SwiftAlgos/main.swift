6bd7d9fdef484351f1d2f54b8ae5f4d2d7bbf627
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2019-12-24 09:08:54 -0500

    setup: add XCode project

---
+++ SwiftAlgos/SwiftAlgos/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgos
+//
+//  Created by Mahesh Khanwalkar on 12/24/19.
+//  Copyright © 2019 Mahesh Khanwalkar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
